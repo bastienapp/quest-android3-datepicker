fd75813080bc57d1431423ec4d6bbd104934ee0f
bastienwcs <bastien@wildcodeschool.fr>
2018-02-14 11:01:47 +0100

    Add DatePickerDialog

--- app/src/main/java/fr/wcs/blablawild/ItinerarySearchActivity.java
+++ app/src/main/java/fr/wcs/blablawild/ItinerarySearchActivity.java
@@ -1,12 +1,18 @@
 package fr.wcs.blablawild;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class ItinerarySearchActivity extends AppCompatActivity {
 
@@ -37,5 +43,36 @@ public class ItinerarySearchActivity extends AppCompatActivity {
                 }
             }
         });
+
+        final Calendar myCalendar = Calendar.getInstance();
+        final EditText dateValue = findViewById(R.id.dateValue);
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel(dateValue, myCalendar);
+            }
+
+        };
+
+        dateValue.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(ItinerarySearchActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void updateLabel(EditText dateValue, Calendar myCalendar) {
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        dateValue.setText(sdf.format(myCalendar.getTime()));
     }
 }
